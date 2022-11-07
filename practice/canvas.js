7cd0c736b4935f9e33a7f520279c7b14f8ee6519
Calvin <calvinvenne@gmail.com>
2022-11-07 16:23:11 -0700

    Challenges and other stuff

---
+++ practice/canvas.js
@@ -0,0 +1,6 @@
+
+let canvas = document.getElementById("canvas-1");
+let context = canvas.getContext("2d");
+
+context.fillRect(50, 50, 12, 12);
+
--- practice/variables.js
+++ practice/variables.js
@@ -6,5 +6,44 @@ let x = 500; // set x to have the value 500
 let y = 276;
 
 let sum = x + y;
-
 let pi = 3.14;
+
+console.log("x is ", x);
+
+let myName = "Calvin Venne";
+
+// strings have a length property
+myName.length;
+
+let whatWillHappen = myName + x;
+
+console.log(whatWillHappen, sum);
+
+let crazMath = 100 * x / (sum + whatWillHappen) - y;
+
+console.log(crazMath);
+
+let isGameOver = false;
+
+console.log(isGameOver, typeof isGameOver);
+
+// arrays can hold lists of values
+let ages = [25, 17, 16, 85];
+
+console.log("ages is an array", ages);
+
+let secondAge = ages[1];
+
+
+let student = {
+	name: "Calvin",
+	grade: "A++ * 1000",
+	gpa: 4.0,
+	wearsAHat: false,
+	friends: ["Me, Myself, and I"]
+};
+
+console.log("student is", student)
+console.log("student name is", student.name)
+
+student.gradeLevel = 9;
